c07dcfec9d28c7eb3616401d54f19444e38a4f20
Andronik Maria <andronikmar@yandex.com>
2021-09-24 18:20:56 +0300

    Бот-ассистентдз

--- homework.py
+++ homework.py
@@ -1,25 +1,42 @@
+import logging
 import os
 import time
+
 import requests
 import telegram
 from dotenv import load_dotenv
-
+from telegram import Bot
+from telegram.ext import Filters, MessageHandler, Updater
 
 load_dotenv()
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO, handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ])
+
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = Bot(token=TELEGRAM_TOKEN)
+
+
+def say_hi(update, context):
+    chat = update.effective_chat
+    logger.info('Send message')
+    text_hi = 'Привет,как будут обновления по домашке - сообщу!!!'
+    context.bot.send_message(chat_id=CHAT_ID,
+                             text=text_hi)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+    if status == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -27,24 +44,43 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+
+    url = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    homework_statuses = requests.get(url, headers=headers, params=payload)
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    current_timestamp = int(time.time())  # Начальное значение timestamp
+
+    current_timestamp = int(time.time())
 
     while True:
         try:
-            ...
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
+            logger.debug('Start')
+            updater = Updater(TELEGRAM_TOKEN, use_context=True)
+            updater.dispatcher.add_handler(MessageHandler
+                                           (Filters.text, say_hi))
+            new_homework = get_homeworks(current_timestamp)
+            if new_homework.get('homeworks'):
+                homework_now = new_homework.get('homeworks')[0]
+                new_status = parse_homework_status(homework_now)
+                logger.info('Send message')
+                send_message(new_status)
+            current_timestamp = new_homework.get(
+                'current_date')
+            updater.start_polling()
+            updater.idle()
+            time.sleep(20 * 60)
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+            logging.error(f'Error {e}!')
+            bot.send_message(chat_id=CHAT_ID, text='error!')
             time.sleep(5)
 
 
